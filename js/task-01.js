4441c9eb6c96cb6c9d77ef974d5a0f1c0a786cb9
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2023-11-16 21:44:41 +0200

    first task done

--- js/task-01.js
+++ js/task-01.js
@@ -1,39 +1,12 @@
-// HTML містить список категорій ul#categories.
+const categ = document.querySelector('#categories'),
+        categItem = document.querySelectorAll('li.item');
 
-// <ul id="categories">
-//   <li class="item">
-//     <h2>Animals</h2>
-//     <ul>
-//       <li>Cat</li>
-//       <li>Hamster</li>
-//       <li>Horse</li>
-//       <li>Parrot</li>
-//     </ul>
-//   </li>
-//   <li class="item">
-//     <h2>Products</h2>
-//     <ul>
-//       <li>Bread</li>
-//       <li>Parsley</li>
-//       <li>Cheese</li>
-//     </ul>
-//   </li>
-//   <li class="item">
-//     <h2>Technologies</h2>
-//     <ul>
-//       <li>HTML</li>
-//       <li>CSS</li>
-//       <li>JavaScript</li>
-//       <li>React</li>
-//       <li>Node.js</li>
-//     </ul>
-//   </li>
-// </ul>
+        console.log(`Кількість категорій li.item ${categItem.length}`);
 
-// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
+categItem.forEach(item => {
+        const categTitle = item.querySelector('h2').textContent,
+                categListLength = item.querySelectorAll('ul > li').length;
 
-// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
-// На що буде звертати увагу ментор при перевірці:
-// Кількість категорій, їх назва та кількість елементів отримані за допомогою властивостей і методів DOM-елементів
-// Дані за кожною категорією були отримані й виведені в консоль у тілі циклу або методу forEach()
+        console.log(categTitle);
+        console.log(categListLength);
+})
