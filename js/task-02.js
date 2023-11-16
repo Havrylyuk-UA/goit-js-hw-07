90382d45b27d58d7cc12ef9d123dd122471319ca
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2023-11-16 22:33:15 +0200

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,37 +1,3 @@
-// Напиши скрипт для створення галереї зображень на основі масиву даних. HTML містить список ul.gallery.
-
-// <ul class="gallery"></ul>
-
-// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
-
-// Ти можеш створити й додати HTML-елементи, використовуючи document.createElement() і elem.append() або шаблонні рядки і elem.insertAdjacentHTML().
-
-// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
-// Додай мінімальне оформлення галереї флексбоксами через CSS класи.
-// const images = [
-//   {
-//     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-//     alt: "White and Black Long Fur Cat",
-//   },
-//   {
-//     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-//   },
-//   {
-//     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-//     alt: "Group of Horses Running",
-//   },
-// ];
-
-
-// На що буде звертати увагу ментор при перевірці:
-// Створена й додана в DOM галерея із трьох зображень
-// Галерея додана у список ul.gallery і являє собою 3 елементи <li>, в які вкладені елементи <img>
-// Для створення елементів <img> використані дані з масиву об’єктів images
-// Усі елементи галереї додані в DOM за одну операцію додавання
-// Є мінімальне оформлення галереї флексбоксами через CSS класи
-
-
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -46,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryBox = document.querySelector('.gallery');
+
+const imagesList = images.flatMap(item => 
+  `<li><img src="${item.url}" alt="${item.alt}" /></li>`
+).join('');
+
+galleryBox.innerHTML = imagesList;
